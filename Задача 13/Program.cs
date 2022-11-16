298c29b8a8bfbb824f19f3ad8c57915461d96efc
Denis <ivdenvas@gmail.com>
2022-11-16 17:56:47 +0300

    start task13

---
+++ Задача 13/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if  (num > 99 && num <1000) 
+    Console.Write($"{num} -> {num % 10}");
+else if (num => 1000)
+    Console.Write($"{num} -> {num - ((num / 1000)* 1000)}");
