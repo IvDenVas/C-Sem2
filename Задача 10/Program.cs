3df47ee23827b832e830ba646f45ca70719b2f3e
Denis <ivdenvas@gmail.com>
2022-11-16 17:30:43 +0300

    finish task10

---
+++ Задача 10/Program.cs
@@ -0,0 +1,13 @@
+﻿int number;
+while (true)
+{
+    Console.WriteLine("Введите трёхзначное число: ");
+    if (int.TryParse(Console.ReadLine(), out number))
+        if (number > 99 && number <1000)
+            break;
+    Console.WriteLine("Ошибка ввода");
+}
+
+int b = (number / 10) % 10;
+
+Console.Write($"{number} -> {b}");
