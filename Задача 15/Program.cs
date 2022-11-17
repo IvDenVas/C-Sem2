5b8a40281e88132cd9f099fc85988558c5a9b7ed
Denis <ivdenvas@gmail.com>
2022-11-17 16:47:47 +0300

    finish Task15

---
+++ Задача 15/Program.cs
@@ -0,0 +1,13 @@
+﻿int number;
+while (true)
+{
+    Console.Write("Введите день недели: ");
+    if (int.TryParse(Console.ReadLine(), out number))
+        if (number > 0 && number <8)
+            break;
+    Console.WriteLine("Ошибка, в неделе 7 дней!!!!");
+}
+if (number > 5)
+    Console.Write($"{number} -> да");
+else
+    Console.Write($"{number} -> нет");
