2a5e9c42f1bb5548cde9a3c006b1ce5b881d20db
Denis <ivdenvas@gmail.com>
2022-11-17 17:32:29 +0300

    finish task 13

--- Задача 13/Program.cs
+++ Задача 13/Program.cs
@@ -1,6 +1,47 @@
-﻿Console.Write("Введите число: ");
-int num = int.Parse(Console.ReadLine());
-if  (num > 99 && num <1000) 
-    Console.Write($"{num} -> {num % 10}");
-else if (num => 1000)
-    Console.Write($"{num} -> {num - ((num / 1000)* 1000)}");
+﻿int num;
+while (true)
+{
+    Console.WriteLine("Введите число: ");
+    if (int.TryParse(Console.ReadLine(), out num))
+        break;
+    Console.WriteLine("Ошибка ввода");
+}
+int num1 = 0;
+int num2 = num;
+
+if ( num > 99 && num < 1000)
+{
+    num1 = num % 10;
+    Console.Write($"{num} -> {num1}");
+}
+else if (num < 100)
+{
+    Console.Write($"{num} -> третьей цифры нет");
+}
+else 
+{
+    while (num2 > 999)
+    {
+        num2 = num2 / 10;
+        num1 = num2 % 10;
+  
+    }
+Console.Write($"{num} -> {num1}");
+}
+
+      
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
